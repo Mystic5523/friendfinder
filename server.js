d7bbc9860f0f452537f4fc1621694dc2ecda52d4
Mystic5523 <Mystic5523@hotmail.com>
2018-08-13 20:22:27 -0500

    i don't know

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// var pat = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // var p = path.join(__dirname, '/routing/apiRoutes.js')
 // var o = path.join(__dirname, '/routing/htmlRoutes.js')
